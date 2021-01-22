2b921d4c885ce0500c08c14a3e7fb314459108a8
4tj <csdk@outlook.com>
2021-01-22 12:55:31 +0800

    initial

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import fastifyCookie from 'fastify-cookie';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  app.register(fastifyCookie);
+  await app.listen(3000);
+}
+bootstrap();
